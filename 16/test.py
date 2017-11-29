2883d863483a27fa5f00b3ba7edeb58ab75f334b
Liu JC <18796559783@163.com>
2017-11-29 10:19:34 +0800

    16
    shixi 16

---
+++ 16/test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2017/11/28 16:47
+# @Author  : Jsen617
+# @Version    : 3.6
+# @File    : test.py
+# @Software: PyCharm
+class test(object):
+    def __init__():
+        pass
+
+
+if __name__ == "__main__":
+    pass
